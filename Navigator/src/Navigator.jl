c19b080d858848d5a4a4b5de4f00f8fa16cb2b8e
ivansladoljev <144113061+ivansladoljev@users.noreply.github.com>
2023-10-11 11:55:22 +0200

    added names

--- Navigator/src/Navigator.jl
+++ Navigator/src/Navigator.jl
@@ -2,17 +2,17 @@ module Navigator
 include("power_spectrum.jl")
 include("mapmaker.jl")
 include("analysis.jl")
-include("download_data.jl")
 
 using .power_spectrum
 using .mapmaker
 using .analysis
 
-export CMBcl,linl, CMBdl, noise_spectrum,derivativeDl
+export get_CMBcl,linl, get_CMBdl, get_noise_spectrum, derivativeDl
 export plot_CMB, vary_amplitude,vary_H0,vary_index,vary_omegab,vary_omegac,vary_tau
-export derivative_of_amplitude,derivative_of_H0,derivative_of_index,derivative_of_omegab,derivative_of_omegac,derivative_of_tau
-export alm,Map, Noise, NoisyMap
-export clestimate,clobserved,dlestimate,dlobserved
+export plot_derivative_of_amplitude, plot_derivative_of_H0, plot_derivative_of_index, plot_derivative_of_omegab, plot_derivative_of_omegac, plot_derivative_of_tau
+export get_alm, make_map, make_noise, make_noisymap
+export get_clestimate, get_clobserved, get_dlestimate, get_dlobserved
 
 
 end
+
